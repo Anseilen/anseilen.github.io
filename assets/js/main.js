beb19380ec2ba5a131a60c87859f8181511da5d0
changdae lee <changdae.lee93@gmail.com>
2020-07-02 15:40:38 +0900

    [imprv] windows 64, 32비트 구분

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,10 +114,15 @@
 
 })(jQuery);
 
-if (navigator.appVersion.indexOf("Win") != -1) {
-  console.log("windows");
-  document.getElementById("download-windows").style.display = "block";
+const appVersion = navigator.appVersion;
+if (appVersion.indexOf("Win") != -1 && (appVersion.indexOf("x64") != -1 || appVersion.indexOf("Win64") != -1 || appVersion.indexOf("WOW64") != -1)) {
+  console.log("windows 64");
+  document.getElementById("download-windows-64").style.display = "block";
+} else if (appVersion.indexOf("Win") != -1) {
+  console.log("windows 32");
+  document.getElementById("download-windows-32").style.display = "block";
 }
+
 if (navigator.appVersion.indexOf("Mac") != -1) {
   console.log("Mac");
   document.getElementById("download-mac").style.display = "block";
